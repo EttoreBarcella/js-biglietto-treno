617d22adc1895f0231e268c39ec66cc12f415aee
EttoreBarcella <ettore.barcella@gmail.com>
2022-04-28 16:07:31 +0200

    added else

--- js/main.js
+++ js/main.js
@@ -13,13 +13,14 @@ const y = Number(prompt("Buongiorno, quanti anni ha?"))
 // 3. Calcolo il prezzo del biglietto sapendo che OGNI km = 0,21 euro e il risultato deve avere formato 0,00 euro:
 // SE età < di 18 avrò >>> N. KM 20%
 // SE età > di 65 avrò >>> N KM 40%
-let z = 0.21 * x
-let zDecimal = z.toFixed(1)
-console.log(z)
+let z = 0.21 * x.toFixed(1)
+
 if (y < 18){
-    console.log(( zDecimal * 20) / 100)
+    console.log(( z * 20) / 100)
 
 }
 if (y > 65){    
-    console.log(( zDecimal * 40) / 100)
+    console.log(( z * 40) / 100)
+} else{
+    console.log(z)
 }
